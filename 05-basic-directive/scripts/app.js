dc6a43287d2b10df9951c71d569df5f8514425ac
joshin-run <jahmsc@gmail.com>
2017-04-26 12:50:05 -0600

    Feat (main) All directories in working order

--- 05-basic-directive/scripts/app.js
+++ 05-basic-directive/scripts/app.js
@@ -1,14 +1,9 @@
-"use strict"
-
 var app = angular.module('app', []);
 
 app.directive("superDuck", function() {
   return {
     restrict: 'EAC',
-    // determines which types you support: 'EAC'...
-    // element, attribute, class...
-    scope: { // isolate scope
-    // allows to grab
+    scope: {
       'duckname': '@'
     },
     link: function(scope, element, attrs) {
@@ -16,8 +11,6 @@ app.directive("superDuck", function() {
     // register DOM listeners, exicuted after ... cloned
     // link: is a method
     },
-    template: "<div><p>Please tell me your coding woes so we can paddle to the finish line together!</p><p>Yours truwly,
-    {{duckname}}</p></div>"
-    // could point to temp with url instead, at new file
+    template: "<div><p>Please tell me your coding woes so we can paddle to the finish line together!</p><p>Yours truwly, {{duckname}}</p></div>"
   }
 })
